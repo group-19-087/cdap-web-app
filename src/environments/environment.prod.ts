a98c9e676f6ae8ba567cdb6096c407463493efb9
Isuru Hettiarachchi <isuru71@gmail.com>
2019-10-31 00:20:56 +0530

    Production build for final viva

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-const ec2url = 'http://ec2-13-127-182-254.ap-south-1.compute.amazonaws.com:3000/';
+const ec2url = 'http://ec2-13-235-87-37.ap-south-1.compute.amazonaws.com:3000/';
 export const environment = {
   production: true,
   upload_url: ec2url + 'v1/videos/upload',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const ec2url = 'http://ec2-35-154-98-108.ap-south-1.compute.amazonaws.com:3000/';
+const ec2url = 'http://ec2-13-235-87-37.ap-south-1.compute.amazonaws.com:3000/';
 // const ec2url = 'http://localhost:3000/';
 export const environment = {
   production: false,
